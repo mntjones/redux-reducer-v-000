d06cb84f0604fb2a21692475c027095edd159d82
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-05 18:17:09 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,8 +3,7 @@ export function manageFriends(state, action){
     case 'ADD_FRIEND':
       return {friends: [...state.friends, action.friend]};
     case 'REMOVE_FRIEND':
-      debugger;
-      return state.friends.filter( el => el.name !== action.friend);
+      return state.friends.filter( el => el.id !== action.friend.id);
     default:
       return state;
   }
