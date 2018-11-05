20bb4387242c204ca42742ac88b240cd0983a982
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-05 18:22:51 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,6 +4,7 @@ export function manageFriends(state, action){
       return {friends: [...state.friends, action.friend]};
     case 'REMOVE_FRIEND':
       // go through friends array, match id, remove friend, return friends array
+      
       return 
     default:
       return state;
